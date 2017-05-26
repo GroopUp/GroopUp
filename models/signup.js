697ec92bdfaaf6e1f010d41759f710131baf39ad
Clinton Diamond <clintondiamond@Clinton-Diamonds-MacBook-Pro.local>
2017-05-26 12:18:26 -0700

    updated models

---
+++ models/signup.js
@@ -0,0 +1,31 @@
+module.exports = function(sequelize, Datatypes){
+	var Signup = sequelize.define("Signup", {
+
+		timestamps: true
+	},
+	    {
+      
+      classMethods: {
+        associate: function(models) {
+          
+          Signup.belongsTo(models.User, {
+            foreignKey: {
+              allowNull: false
+            }
+          });
+        }
+      },
+      classMethods: {
+        associate: function(models) {
+          
+          Signup.belongsTo(models.Event, {
+            foreignKey: {
+              allowNull: false
+            }
+          });
+        }
+      }
+    }
+	);
+	return Event;
+}
