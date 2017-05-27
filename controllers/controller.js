4ea2904cb3b4062edb73c117b43195d855102c6f
bluffingtuna@gmail.com <bluffingtuna@gmail.com>
2017-05-27 16:49:02 -0700

    merging

--- controllers/controller.js
+++ controllers/controller.js
@@ -5,7 +5,9 @@ var router = express.Router();
 
 router.get('/', function(req, res) {
     db.Event.findAll({
-    	order: [['createdAt', 'DESC']]
+        order: [
+            ['createdAt', 'DESC']
+        ]
     }).then(function(data) {
         var hbsObject = {
             event: data
@@ -21,29 +23,30 @@ router.get('/view-event/:id', function(req, res) {
         },
         include: [db.User]
     }).then(function(data) {
-    	res.json(data);
+        res.json(data);
     });
 });
 
-router.get("/new-user", function(req, res){
+router.get("/new-user", function(req, res) {
     res.render("new-user");
 })
 
-router.get("/new-business", function(req, res){
+router.get("/new-business", function(req, res) {
     res.render("new-business");
 })
 
 router.post('/new-user', function(req, res) {
     console.log(req.body);
     db.User.create({
-    	name: req.body.name,
-    	password: req.body.password,
-    	email: req.body.email,
-    	age: req.body.age,
-    	sex: req.body.sex,
-    	picture: req.body.picture
+        name: req.body.name,
+        password: req.body.password,
+        email: req.body.email,
+        age: req.body.age,
+        sex: req.body.sex,
+        picture: req.body.picture
     }).then(function(data) {
-    	res.json(data);
+        res.json(data);
+
     });
 });
 
@@ -54,14 +57,19 @@ router.post('/new-user', function(req, res) {
 
 router.post('/new-business', function(req, res) {
     db.Business.create({
-    	name: req.body.name,
-    	password: req.body.password,
-    	phonenumber: req.body.phonenumber,
-    	email: req.body.email,
-        picture: req.body.picture}).then(function(data) {
-    	res.json(data);
+        name: req.body.name,
+        password: req.body.password,
+        phonenumber: req.body.phonenumber,
+        email: req.body.email,
+        picture: req.body.picture
+    }).then(function(data) {
+        //    console.log("data values", data.dataValues);
+        // res.render("my-business", data.dataValues)
+        res.render("new-event");
     });
 });
+
+
 
 // !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 // router.get('/business-login', function(req, res) {
@@ -74,6 +82,8 @@ router.post('/new-business', function(req, res) {
 // });
 
 router.post('/new-event', function(req, res) {
+    console.log("req", req)
+    console.log("res", res)
     db.Event.create({
         title: req.body.title,
         location: req.body.location,
@@ -82,19 +92,20 @@ router.post('/new-event', function(req, res) {
         picture: req.body.picture,
         attendance_cap: req.body.attendance_cap
     }).then(function(data) {
-    	res.json(data);
+        console.log("data", data)
+        res.json(data);
     });
 });
 
 // TEST
 router.get('/user-account/:id', function(req, res) {
     db.Signup.findAll({
-    	where: {
-    		id: req.body.id
-    	},
-    	include: [db.User, db.Event]
+        where: {
+            id: req.body.id
+        },
+        include: [db.User, db.Event]
     }).then(function(data) {
-    	res.json(data);
+        res.json(data);
     });
 });
 
@@ -105,12 +116,12 @@ router.get('/user-account/:id', function(req, res) {
 
 router.get('/business-account/:id', function(req, res) {
     db.Business.findOne({
-    	where: {
-    		id: req.body.id
-    	}, 
-    	include: [db.Event]
+        where: {
+            id: req.body.id
+        },
+        include: [db.Event]
     }).then(function(data) {
-    	res.json(data);
+        res.json(data);
     });
 });
 
